c2d3a1f8dab80cdf852515d6f148b94a68770836
Lauren Fernandez <lfernan60@ufl.edu>
2018-04-13 14:09:51 -0400

    Add the tingle jQuery plugin

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-// When the user scrolls the page, execute myFunction
+//Sticky Nav makes my nav bar show at all times as you scroll down the site
+
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -15,3 +13,29 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+//Makes the modal at the bottom of the page
+    var modalTinyNoFooter = new tingle.modal({
+        onClose: function() {
+            console.log('close');
+        },
+        onOpen: function() {
+            console.log('open');
+        },
+        beforeOpen: function() {
+            console.log('before open');
+        },
+        beforeClose: function() {
+            console.log('before close');
+            return true;
+        },
+        cssClass: ['class1', 'class2']
+    });
+    var btn = document.querySelector('.js-tingle-modal-1');
+    btn.addEventListener('click', function(){
+        modalTinyNoFooter.open();
+    });
+    modalTinyNoFooter.setContent(document.querySelector('.tingle-demo-tiny').innerHTML);
+
+//X
